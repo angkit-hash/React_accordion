b6bcf3b09a234d6a25fddc0c078fed604be7802c
angkit93@gmail.com <angkit93@gmail.com>
2024-04-09 15:17:09 +0530

    fix:alignment

--- src/Data.js
+++ src/Data.js
@@ -1,24 +1,51 @@
-import React, { useState }  from 'react'
+import React, { useState } from 'react'
 import "./App.css"
-import { FcExpand,FcCollapse} from 'react-icons/fc';
+import { FcExpand, FcCollapse } from 'react-icons/fc';
 
-const Data = ({title,body}) => {
+const Data = ({ title, body }) => {
 
-   const [showCont , showContM] = useState(false)
+    const [showCont, showContM] = useState(false)
 
-   const modify = () =>{
-     showContM(!showCont)
+    const modify = () => {
+        showContM(!showCont)
 
-   }
-    return(
+    }
+    return (
 
         <div className="accordion-item" onClick={modify}>
             <div className='acordion-title'>
-            <h5>{title}</h5>
-                   <p><FcExpand /> </p>                                  
+
+                {showCont ? (
+                   <div> 
+                    <div class="container">
+                        <div class="first-element" >
+
+                            <h2>{title}</h2>
+
+                        </div>
+                        <div class="second-element" >
+
+                            <p><FcCollapse /></p>
+
+                        </div>
+                        
+
+                    </div>
+
+                    <div>
+
+                        <p>{body}</p>
+                        </div>
+                    </div>
+                   
+
+                ) : (
+                    <div className='acordion-title'    >
+                        <h5>{title}</h5>
+                        <p ><FcExpand /></p>
+                    </div>
+                )}
             </div>
-          
-            {showCont && <p>{body}</p>}
         </div>
     )
 
